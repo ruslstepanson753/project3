c3a3f978dbae951f6fb76ebe30f658f599ed7746
ruslstepanson753 <ruslstepanson753@gmail.com>
2025-08-28 15:34:27 +0300

    custom login view

--- src/main/java/com/stepanov/project03/config/SecurityConfig.java
+++ src/main/java/com/stepanov/project03/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.stepanov.project03.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -30,6 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureForwardUrl("/auth/login?error");
     }
 
     @Bean
